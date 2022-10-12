a25e4de8e2d7d04fbb2a1f1d4fc41904ce42f2ed
Aleksandar Sabo <alxsabo@gmail.com>
2022-10-12 17:50:29 +0200

    Disable tests when license not set

--- tests/Test/Issues/RavenDB_15753Test.php
+++ tests/Test/Issues/RavenDB_15753Test.php
@@ -7,11 +7,19 @@ use RavenDB\Documents\Indexes\AdditionalAssembly;
 use RavenDB\Documents\Indexes\AdditionalAssemblySet;
 use RavenDB\Documents\Indexes\IndexDefinition;
 use RavenDB\Documents\Operations\Indexes\PutIndexesOperation;
+use tests\RavenDB\Infrastructure\DisableOnPullRequestCondition;
 use tests\RavenDB\RemoteTestBase;
 use Throwable;
 
 class RavenDB_15753Test extends RemoteTestBase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        DisableOnPullRequestCondition::evaluateExecutionCondition($this);
+    }
+
     /** @doesNotPerformAssertions */
     public function testAdditionalAssemblies_Runtime(): void
     {
