30c139c45578051c6ccaae3f2f2a72ca06e7b0b1
Aleksandar Sabo <alxsabo@gmail.com>
2025-01-28 18:16:02 +0100

    Disable test for v7

--- tests/Test/Issues/RavenDB_11440Test.php
+++ tests/Test/Issues/RavenDB_11440Test.php
@@ -16,6 +16,8 @@ class RavenDB_11440Test extends RemoteTestBase
 {
     public function testCanGetLogsConfigurationAndChangeMode(): void
     {
+        TestRunGuard::disableTestForRaven7AndLater($this);
+
         $store = $this->getDocumentStore();
         try {
             /** @var GetLogsConfigurationResult $configuration */
